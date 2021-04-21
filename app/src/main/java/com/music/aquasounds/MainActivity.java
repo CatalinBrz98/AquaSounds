35757bbb58c4360e978964e2c19ddfcd8f1d0dda
Breaz Catalin <catalin.brz98@yahoo.com>
2021-04-21 07:30:21 +0300

    Music Play, NavBar, Time, Dark Theme

--- app/src/main/java/com/music/aquasounds/MainActivity.java
+++ app/src/main/java/com/music/aquasounds/MainActivity.java
@@ -2,11 +2,13 @@ package com.music.aquasounds;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.ActivityManager;
 import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
@@ -16,6 +18,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import java.io.File;
@@ -31,10 +34,12 @@ public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_CODE = 9257;
     private boolean initialized;
     private List<String> musicFilesList;
+    private final MediaPlayer mp = new MediaPlayer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
         setContentView(R.layout.activity_main);
     }
 
@@ -59,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @SuppressWarnings("ResultOfMethodCallIgnored")
     private void addMusicFilesFrom(String dirPath) {
         final File musicDir = new File(dirPath);
         if(!musicDir.exists()) {
@@ -69,14 +75,14 @@ public class MainActivity extends AppCompatActivity {
         List<String> extensions = Arrays.asList("mp3", "wav");
         for(File file : files) {
             final String filePath = file.getAbsolutePath();
-            Log.d("filePathDebug",filePath);
+            Log.d("filePathDebug", filePath);
             if (filePath.lastIndexOf("/") == -1)
                 continue;
-            final String fileName = filePath.substring(filePath.lastIndexOf("/")+1);
+            final String fileName = filePath.substring(filePath.lastIndexOf("/") + 1);
             if (fileName.lastIndexOf(".") == -1)
                 continue;
-            final String extension = fileName.substring(fileName.lastIndexOf(".")+1);
-            Log.d("extensionDebug",extension);
+            final String extension = fileName.substring(fileName.lastIndexOf(".") + 1);
+            Log.d("extensionDebug", extension);
             if(extensions.contains(extension))
                 musicFilesList.add(filePath);
         }
@@ -86,6 +92,30 @@ public class MainActivity extends AppCompatActivity {
         musicFilesList.clear();
         addMusicFilesFrom(String.valueOf(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC)));
         addMusicFilesFrom(String.valueOf(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)));
+    }
+
+    private int playMusicFile(String filePath) {
+        try {
+            mp.reset();
+            mp.setDataSource(filePath);
+            mp.prepare();
+            mp.start();
+            Log.d("playingDebug",filePath);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return mp.getDuration();
+    }
+
+    private String durationToString(int duration) {
+        int seconds = duration % 60000 / 1000;
+        int minutes = duration / 60000;
+        String sSeconds = String.valueOf(seconds);
+        if(seconds < 10) {
+            sSeconds = "0" + sSeconds;
+        }
+        String sMinutes = String.valueOf(minutes);
+        return sMinutes + ":" + sSeconds;
     }
 
     @Override
@@ -98,17 +128,64 @@ public class MainActivity extends AppCompatActivity {
         if(!initialized) {
             final ListView listView = findViewById(R.id.listView);
             final TextAdapter textAdapter = new TextAdapter();
+            final SeekBar seekBar = findViewById(R.id.seekBar);
+            final TextView currentMusicTime = findViewById(R.id.currentMusicTime);
+            final TextView totalMusicTime = findViewById(R.id.totalMusicTime);
             musicFilesList = new ArrayList<>();
             fillMusicList();
             textAdapter.setData(musicFilesList);
             listView.setAdapter(textAdapter);
+            seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+                    mp.seekTo(seekBar.getProgress());
+                }
+            });
+            mp.setOnCompletionListener(mediaPlayer -> seekBar.setVisibility(View.GONE));
+            listView.setOnItemClickListener((adapterView, view, i, l) -> {
+                final String musicFilePath = musicFilesList.get(i);
+                final int songDuration = playMusicFile(musicFilePath);
+                seekBar.setMax(songDuration);
+                runOnUiThread(() -> {
+                    currentMusicTime.setText(durationToString(0));
+                    currentMusicTime.setText(durationToString(mp.getCurrentPosition()));
+                });
+                runOnUiThread(() -> totalMusicTime.setText(durationToString(songDuration)));
+                new Thread() {
+                    @SuppressWarnings("BusyWait")
+                    public void run() {
+                        while(mp.isPlaying()) {
+                            try {
+                                Thread.sleep(1000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            runOnUiThread(() -> {
+                                currentMusicTime.setText(durationToString(mp.getCurrentPosition()));
+                                seekBar.setProgress(mp.getCurrentPosition());
+                            });
+                        }
+                    }
+                }.start();
+                seekBar.setVisibility(View.VISIBLE);
+            });
             initialized = true;
         }
     }
 }
 
 class TextAdapter extends BaseAdapter {
-    private List<String> data = new ArrayList<String>();
+    private final List<String> data = new ArrayList<>();
 
     @Override
     public int getCount() {
@@ -129,7 +206,7 @@ class TextAdapter extends BaseAdapter {
     public View getView(int i, View convertView, ViewGroup parent) {
         if(convertView == null) {
             convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false);
-            convertView.setTag(new ViewHolder((TextView) convertView.findViewById(R.id.myItem)));
+            convertView.setTag(new ViewHolder(convertView.findViewById(R.id.musicItem)));
         }
         ViewHolder holder = (ViewHolder) convertView.getTag();
         final String item = data.get(i);
